1df09bf2b6ab11a3907f47bf21147daf51b7c582
Theodore Zhu <theodorez888@gmail.com>
2025-08-28 16:29:42 -0400

    favicon and react analytics fix

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import NavBar from './components/NavBar.jsx';
 import Gallery from './components/Gallery.jsx';
 import Self from './components/Self.jsx';
 
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react"
 
 // A wrapper that applies fade-out (exit) then fade-in (enter) on route changes
 function FadeRoutes() {
